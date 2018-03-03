6e15704d0b1d7a519513090750f960d8d2ab29b1
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2018-03-03 14:06:39 +0300

    Created class Actor

--- game.js
+++ game.js
@@ -30,3 +30,63 @@ class Vector {
       }
     }
 }
+
+class Actor {
+    constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+        try {
+            if (pos instanceof Vector === false || size instanceof Vector === false || speed instanceof Vector === false) {
+                throw `Неверно передан аргумент`;
+            }
+
+            this.pos = pos;
+            this.size = size;
+            this.speed = speed;
+
+        } catch (error) {
+            console.log(`${error}`);
+        }
+    }
+
+    act() {
+
+    }
+
+    get left() {
+        return this.pos.x;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x;
+    }
+
+    get top() {
+        return this.pos.y;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y;
+    }
+
+    get type() {
+        return 'actor';
+    }
+    isIntersect(item) {
+        try {
+            if (item instanceof Actor === false) {
+                throw `Неверно передан аргумент`;
+            }
+            if (item === this) {
+                return false;
+            }
+            if (this.left === item.left && this.right === item.right && this.top === item.top && this.bottom === item.bottom) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        } catch (error) {
+            console.log(`${error}`);
+        }
+    }
+}
